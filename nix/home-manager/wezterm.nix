99304d804746fdeaec8a32b49f4d277f24afb59d
gesop0n <ishikuro6.2@gmail.com>
2026-05-10 00:08:36 +0900

    feat: WezTermでフォントのサイズを指定

--- nix/home-manager/wezterm.nix
+++ nix/home-manager/wezterm.nix
@@ -2,5 +2,12 @@
 {
   programs.wezterm = {
     enable = true;
+    extraConfig = ''
+      local config = wezterm.config_builder()
+
+      config.font_size = 16.0
+
+      return config
+    '';
   };
 }
