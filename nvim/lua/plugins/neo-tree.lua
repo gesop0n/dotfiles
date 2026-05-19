a9824b3fc9d60e27485339a99f2775a0f846a237
gesop0n <ishikuro6.2@gmail.com>
2026-05-19 15:00:42 +0900

    feat: Neotreeで .git/ や .DS_Storeは表示されないようにする

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,10 @@ return {
         visible = true,
         hide_dotfiles = false,
         hide_gitignored = false,
+        never_show = {
+          ".DS_Store",
+          ".git",
+        },
       },
     },
   },
