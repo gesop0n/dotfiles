da47b2200388235ceb84b85cef7026c446ae7134
gesop0n <ishikuro6.2@gmail.com>
2026-09-21 01:13:16 +0900

    使わなくなった dataform.nvim を削除する

--- nix/home-manager/nixvim/plugins/custom.nix
+++ nix/home-manager/nixvim/plugins/custom.nix
@@ -3,15 +3,6 @@
   programs.nixvim = {
     extraPlugins = [
       (pkgs.vimUtils.buildVimPlugin {
-        name = "dataform-nvim";
-        src = pkgs.fetchFromGitHub {
-          owner = "magal1337";
-          repo = "dataform.nvim";
-          rev = "43a5f9e17275325ae32e5248c6f21636418e2018";
-          hash = "sha256-HA6E9L6U37Btb/dDgtm7du97/fae5IawCSg/nmB/tqg=";
-        };
-      })
-      (pkgs.vimUtils.buildVimPlugin {
         name = "wmnusmv-vim";
         src = pkgs.fetchFromGitHub {
           owner = "wannesm";
