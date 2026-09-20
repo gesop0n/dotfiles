cd426f0eab746e29c5fbae35d88976e01b612e8a
gesop0n <ishikuro6.2@gmail.com>
2026-09-21 01:13:15 +0900

    nvim のファイル/フォルダアイコンを Material Design Icons に寄せる
    nvim-web-devicons は nf-dev-* 中心のため、nf-md-* を優先する mini.icons へ
    置き換える。mockDevIcons で nvim-web-devicons を emulate するので、
    neo-tree / bufferline / lualine / fzf-lua も追従する。
    
    neo-tree のフォルダアイコンは devicons 経由ではなくハードコード値
    (folder_closed/open が nf-fa-*) なので、nf-md-* で明示的に上書きした。

--- nix/home-manager/nixvim/plugins/explorer.nix
+++ nix/home-manager/nixvim/plugins/explorer.nix
@@ -4,6 +4,14 @@
     enable = true;
     # サイドバーの幅（デフォルトは40）
     settings.window.width = 30;
+    # フォルダアイコンは devicons 経由ではなく neo-tree のハードコード値なので、
+    # nf-md-* (Material Design Icons) に揃えて明示的に上書きする。
+    settings.default_component_configs.icon = {
+      folder_closed = "󰉋";
+      folder_open = "󰝰";
+      folder_empty = "󰉖";
+      folder_empty_open = "󰷏";
+    };
     settings.filesystem = {
       filtered_items = {
         visible = true;
--- nix/home-manager/nixvim/plugins/ui.nix
+++ nix/home-manager/nixvim/plugins/ui.nix
@@ -3,7 +3,17 @@
   programs.nixvim.plugins = {
     lualine.enable = true;
     bufferline.enable = true;
-    web-devicons.enable = true;
     indent-blankline.enable = true;
+
+    # ファイルアイコンは mini.icons に任せる。
+    # nvim-web-devicons が nf-dev-* 中心なのに対し、mini.icons は
+    # nf-md-* (Material Design Icons) を優先するため Material 寄りの見た目になる。
+    # mockDevIcons で nvim-web-devicons を emulate するので、
+    # neo-tree / bufferline / lualine / fzf-lua もそのまま追従する。
+    mini-icons = {
+      enable = true;
+      mockDevIcons = true;
+      settings.style = "glyph";
+    };
   };
 }
